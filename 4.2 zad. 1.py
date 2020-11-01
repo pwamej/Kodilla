e4712b52a13319754147dab741ff9442d73af534
awamej <a.wamej@gmail.com>
2020-11-01 16:37:15 +0100

    Add is_palindrome function

--- 4.2 zad. 1.py
+++ 4.2 zad. 1.py
@@ -1,1 +1,17 @@
-#start
+def is_palindrome(word):
+    """
+        Prints whether a word is a palindrome, based on argument passed
+        Argument:
+        word
+    """
+    word = word.lower()
+    word_len = len(word)
+    for index in range(word_len):
+        if word[index] != word[-index-1]:
+            return f"{word.capitalize()} is not a palindrome."
+    return f"{word.capitalize()} is a palindrome."
+
+
+print(is_palindrome("Anna"))
+print(is_palindrome("potop"))
+print(is_palindrome("bootcamp"))
