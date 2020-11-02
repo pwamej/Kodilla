c51e6354e9610d805f1b38f231579928b16e5b6c
awamej <a.wamej@gmail.com>
2020-11-02 11:30:33 +0100

    Fix and simplify is_palindrome function

--- 4.2 zad. 1.py
+++ 4.2 zad. 1.py
@@ -1,17 +1,18 @@
 def is_palindrome(word):
     """
-        Prints whether a word is a palindrome, based on argument passed
+        Prints whether a word or sentence is a palindrome, based on argument passed
         Argument:
         word
     """
-    word = word.lower()
-    word_len = len(word)
-    for index in range(word_len):
-        if word[index] != word[-index-1]:
-            return f"{word.capitalize()} is not a palindrome."
-    return f"{word.capitalize()} is a palindrome."
+    lower_word = word.lower()
+    replaced_word = lower_word.replace(" ", "")
+    if replaced_word != replaced_word[::-1]:
+        return f"{word.capitalize()} is not a palindrome."
+    else:
+        return f"{word.capitalize()} is a palindrome."
 
 
 print(is_palindrome("Anna"))
 print(is_palindrome("potop"))
 print(is_palindrome("bootcamp"))
+print(is_palindrome("A to kiwi zdziwi kota"))
